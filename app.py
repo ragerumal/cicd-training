102f12b83bb110a765e807af54d970f17f69c88a
raghunath <raghunath.e@gmail.com>
2025-08-16 15:57:55 -0700

    updated code and tests

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+
 from flask import Flask
 import socket
 
 app = Flask (__name__)
 
+@app.route('/hello')
+def hello():
+    return 'Hello Mark!'
+
 @app.route ('/')
 def home():
     hostname = socket.gethostname()
-    print(hostname)
     ip_address = socket.gethostbyname(hostname)
-    print(ip_address)
     return f'Hello World @{ip_address}'
 
 if __name__ == '__main__':
